9339b4a25e2da630991974be6fceec13b3bc3253
Andre Brunner <andre.brunner70@gmail.com>
2023-01-23 18:04:26 +0100

    variationCache

--- test.py
+++ test.py
@@ -4,26 +4,32 @@ import chess.engine
 
 def evalBoard(board):
   engine = chess.engine.SimpleEngine.popen_uci("C:/Stockfish/stockfish_15.1_win_x64_popcnt/stockfish-windows-2022-x86-64-modern.exe")
-  info = engine.analyse(board, chess.engine.Limit(time=1.0))
+  info = engine.analyse(board, chess.engine.Limit(depth=20))
   engine.quit()
   return info["score"]
 
+def evaluteAndPrint(board):
+  if board.fen() in positionCache.keys():
+    return positionCache[board.fen()]
+  score = evalBoard(board)
+  print(board)
+  score_cp = score.white().cp
+  print(score_cp)
+  positionCache[board.fen()] = score_cp
+  return score_cp
+  
 
 def traverseVariations(variation, mainMove, board) :
-  score = evalBoard(board)
-  print("before: ")
-  score_before = score.white().cp
-  print(score_before)
+  print('##################################')
+  score_before = evaluteAndPrint(board)
   board.push(mainMove) 
   print('----------------------------------')
-  print(board)
-  score = evalBoard(board)
-  print("after: ")
-  score_after = score.white().cp
-  print(score_after)
+  score_after = evaluteAndPrint(board)
   if abs(score_after - score_before) > 100:
     print('??????????')
-  print('----------------------------------')
+    print(mainMove.uci())
+    print(board.fen())
+  print('##################################')
   for childVaratiation in variation.variations:
     varBoard = chess.Board(board.fen())
     traverseVariations(childVaratiation, childVaratiation.move,  varBoard)  
@@ -34,6 +40,10 @@ pgn = open("C:/Users/andre/develop/chessTraverse/x.pgn")
 first_game = chess.pgn.read_game(pgn)
 
 board = first_game.board()
+
+positionCache = {
+  '' : 0
+}
 
 traverseVariations(first_game.variations[0], first_game.variations[0].move, board)
 
