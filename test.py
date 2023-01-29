12e8086917ac34c11483b4f28e782b8b105cb0ff
Andre Brunner <andre.brunner70@gmail.com>
2023-01-29 19:15:55 +0100

    Added commandline and more verbose eval-info

--- test.py
+++ test.py
@@ -1,32 +1,69 @@
 import chess
 import chess.pgn
 import chess.engine
+import sys
+import getopt
+
+#-i
+inputFile = "C:/Users/andre/develop/chessTraverse/x.pgn"
+#-o
+outputFile="zzz.pgn"
+#-d
+engineDepth=1
+#-e
+enginePath="C:\Stockfish\stockfish_15.1_win_x64_popcnt\stockfish-windows-2022-x86-64-modern.exe"
+# total arguments
+n = len(sys.argv)
+print("Total arguments passed:", n)
+# Arguments passed
+print("\nName of Python script:", sys.argv[0])
+
+argv = sys.argv[1:]
+opts, args = getopt.getopt(argv, 'i:o:d:e:')
+for o in opts:
+    print(o)      
+    if (o[0] == '-i'):
+        inputFile = o[1]
+    if (o[0] == '-o'):
+        outputFile = o[1]
+    if (o[0] == '-d'):
+        engineDepth = o[1]
+    if (o[0] == '-e'):
+        enginePath = o[1]
+
+print('Inputfile:' + inputFile)        
+print('Outputfile:' + outputFile)        
+print('Depth:' + str(engineDepth))        
+print('Enginepath:' + enginePath)        
 
 def evalBoard(board):
-  engine = chess.engine.SimpleEngine.popen_uci("C:/Stockfish/stockfish_15.1_win_x64_popcnt/stockfish-windows-2022-x86-64-modern.exe")
-  info = engine.analyse(board, chess.engine.Limit(depth=18))
+  engine = chess.engine.SimpleEngine.popen_uci(enginePath)
+  info = engine.analyse(board, chess.engine.Limit(depth=engineDepth))
   engine.quit()
-  return info["score"]
+  return info
 
 def evaluteAndPrint(board):
   if board.fen() in positionCache.keys():
     return positionCache[board.fen()]
-  score = evalBoard(board)
+  info = evalBoard(board)
   print(board)
-  score_cp = score.white().cp
+  score_cp = info.get('score').white().cp
   print(score_cp)
-  positionCache[board.fen()] = score_cp
-  return score_cp
+  positionCache[board.fen()] = info
+  return info
   
 
 def traverseVariations(variation, mainMove, board) :
   print('##################################')
-  score_before = evaluteAndPrint(board)
+  info_before = evaluteAndPrint(board)
+  score_before = info_before.get('score').white().cp
+  eventual_Variation = board.variation_san(info_before.get('pv')) 
   board.push(mainMove) 
   print('----------------------------------')
-  score_after = evaluteAndPrint(board)
+  info_after = evaluteAndPrint(board)
+  score_after = info_after.get('score').white().cp
   if abs(score_after - score_before) > 100:
-    variation.comment = variation.comment + ' -### ' +str(score_after) +' vs. ' + str(score_before) +' ###-' 
+    variation.comment = variation.comment + ' -### ' +str(score_after) +' vs. ' + str(score_before) + eventual_Variation + ' ###-' 
     print('??????????')
     print(mainMove.uci())
     print(board.fen())
@@ -38,7 +75,7 @@ def traverseVariations(variation, mainMove, board) :
     traverseVariations(childVaratiation, childVaratiation.move,  varBoard)  
 
     
-pgn = open("C:/Users/andre/develop/chessTraverse/x.pgn")
+pgn = open(inputFile)
 
 first_game = chess.pgn.read_game(pgn)
 
@@ -58,4 +95,4 @@ print(board)
 
 print(first_game)
 
-print(first_game, file=open("zzz.pgn", "w"), end="\n\n")
+print(first_game, file=open(outputFile, "w"), end="\n\n")
